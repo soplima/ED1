711440a0e06077497327695f78f9acb1e3ccae31
Sophia Lima <sop.lima13@gmail.com>
2025-01-27 23:04:44 -0300

    ex7

--- lista7/7.C
+++ lista7/7.C
@@ -7,51 +7,19 @@
 using namespace std;
 
 vector<string> vectorize_expression(const string& expression) {
-vector<string>result;
-stringstream ss (expression);
-string token;
+    vector<string>result;
+    stringstream ss (expression);
+    string token;
 
-while (ss >> token)
-{
-    result.push_back(token);
-}
-return result;
+    while(ss >> token){
+        result.push_back(token);
+    }
+    return result;
 }
 
 float calc_posfix(const string& expression) {
     stack<float>operands;
     vector<string> tokens = vectorize_expression(expression);
 
-    for (const string& token : tokens){
-        if(isdigit(token[0])){
-            operands.push(stof(token));
-        } else {
-            if(operands.size() < 2){
-                throw invalid_argument("Expressão inválida: operandos insuficientes.");
-        }
-            float b = operands.top(); operands.pop(); 
-            float a = operands.top(); operands.pop(); 
-
-            if (token == "+") {
-                operands.push(a + b);
-            } else if (token == "-") {
-                operands.push(a - b);
-            } else if (token == "*") {
-                operands.push(a * b);
-            } else if (token == "/") {
-                if (b == 0) { 
-                    throw invalid_argument("Erro: divisão por zero.");
-                }
-                operands.push(a / b);
-            } else {
-                throw invalid_argument("Operador inválido: " + token);
-            }
-        }
-
-    }
-     if (operands.size() != 1) {
-        throw invalid_argument("Expressão inválida.");
-    }
-
-    return operands.top(); 
+    
 }
