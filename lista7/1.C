78ec52bbd56dc75553e856a85bfcf84047a66d5f
Sophia Lima <sop.lima13@gmail.com>
2025-01-27 21:55:32 -0300

    ex2

--- lista7/1.C
+++ lista7/1.C
@@ -2,7 +2,7 @@
 
 std::vector<int> vet_to_vector(int *v, int n){
     std::vector<int>result;
-    for(int i = 0; i < n; i ++){
+    for (int i = 0; i < n; i ++){
         result.push_back(v[i]);
     }
     return result;
--- lista7/2.C
+++ lista7/2.C
@@ -3,19 +3,14 @@
 
 std::forward_list<int> vet_to_flist(int *v, int n)
 {
-    std::forward_list<int> flist;
-
-    if (n == 0 | v == nullptr)
-    {
+    std::forward_list<int>flist;
+    if(n == 0 || v == nullptr){
         return flist;
     }
-
     auto it = flist.before_begin();
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i ++){
         it = flist.insert_after(it, v[i]);
     }
-
     return flist;
 }
