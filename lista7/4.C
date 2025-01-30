0e6e633b274f51d0e267bca04ea8eb47b4f001c7
Sophia Lima <sop.lima13@gmail.com>
2025-01-30 00:26:24 -0300

    ex4

--- lista7/4.C
+++ lista7/4.C
@@ -4,9 +4,8 @@ using namespace std;
 
 vector<int> list_concat(forward_list<int> &list1, forward_list<int> &list2)
 {
-    vector<int>result;
-
-    for(int val : list1){
+    vector<int>result; 
+    for(int val: list1){
         result.push_back(val);
     }
     for(int val: list2){
