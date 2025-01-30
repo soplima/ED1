85b66a4d398674b5f38fd156271524b81bb30e30
Sophia Lima <sop.lima13@gmail.com>
2025-01-30 00:49:07 -0300

    ex5

--- lista7/5.C
+++ lista7/5.C
@@ -4,21 +4,22 @@ using namespace std;
 
 bool check_brackets(const string &expression)
 {
-    stack<char> s;
-    for (char ch : expression) {
-        if (ch == '(' || ch == '[' || ch == '{') {
+    stack<char>s;
+    for(char ch : expression){
+        if (ch == '(' || ch == '[' || ch == '{'){
             s.push(ch);
-        } else if (ch == ')' || ch == ']' || ch == '}') {
-            if (s.empty()) {
+        }
+        else if (ch == ')' || ch == ']' || ch == '}'){
+            if (s.empty()){
                 return false;
             }
             char top = s.top();
             s.pop();
-            if ((ch == ')' && top != '(') ||
-                (ch == ']' && top != '[') ||
-                (ch == '}' && top != '{')) {
+            if((ch == ')' && top != '(') ||
+               (ch == ']' && top != ']') ||
+               (ch == '}' && top != '}')){
                 return false;
-            }
+               }
         }
     }
     return s.empty();
