dafe97220bc2fbb5011cf7700da7624e477a3c4e
Sophia Lima <sop.lima13@gmail.com>
2025-01-30 00:20:27 -0300

    ex3

--- lista7/3.C
+++ lista7/3.C
@@ -3,9 +3,9 @@ using namespace std;
 
 stack<int> vet_to_stack(int *v, int n)
 {
-  stack<int>result;
-  for (int i = 0; i < n; i ++){
-        result.push(v[i]);
+  stack<int>s;
+  for (int i = 0; i < n; i++){
+    s.push(v[i]);
   }
-  return result;
+  return s;
 }
