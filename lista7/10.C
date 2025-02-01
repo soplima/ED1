483f6ee033ba560ed03b246701d9b78f4715337a
Sophia Lima <sop.lima13@gmail.com>
2025-02-01 20:17:00 -0300

    ex10

--- lista7/10.C
+++ lista7/10.C
@@ -7,39 +7,37 @@ using namespace std;
 
 string posfix_to_infix(string expression)
 {
-    stack<string> operand;
+    stack<string>operand;
+
     int i = 0;
-    while (i < expression.length())
-    {
+    while(i < expression.length()){
+
         char token = expression[i];
-        if (token == ' ')
-        {
+        if (token == ' '){
             i++;
             continue;
         }
-        if (isdigit(token))
-        {
+
+        if(isdigit(token)){
             string num = "";
-            while (i < expression.length() && isdigit(expression[i]))
-            {
+            while(i < expression.length() && isdigit(expression[i])){
                 num += expression[i];
                 i++;
             }
             operand.push(num);
         }
-        else if(token == '+' || token == '-' || token == '*' || token == '/'){
-            if (operand.size() < 2) {
-                throw invalid_argument("Expressão posfixa inválida.");
-            }
-            string operand2 = operand.top(); operand.pop();
-            string operand1 = operand.top(); operand.pop();
-            string infix_exp = "(" + operand1 + " " + token + " " + operand2 + ")";
-            operand.push(infix_exp);
-            i++;
+        else if (token == '+'|| token == '-'|| token == '*'|| token == '/'){
+            operand.size() < 2;
+            throw invalid_argument("Expressão posfixa inválida.");
         }
-        if (operand.size() != 1) {
-        throw invalid_argument("Expressão posfixa inválida.");
-        }
+        string operand2 = operand.top(); operand.pop();
+        string operand1 = operand.top(); operand.pop();
+        string infix_exp = "(" + operand1 + " " + token + " " + operand2 + ")";
+        operand.push(infix_exp);
+        i++;
+    }
+    if(operand.size() != 1){
+        throw invalid_argument ("Expressão posfixa inválida.");
     }
     return operand.top();
 }
