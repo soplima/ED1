a417fc5d34b421b1b35663aa1436da90f6d1174e
Sophia Lima <sop.lima13@gmail.com>
2025-02-01 19:48:52 -0300

    ex9

--- lista7/9.C
+++ lista7/9.C
@@ -5,42 +5,43 @@
 #include <stdexcept>
 using namespace std;
 
-float apply_operator(float a, float b, char op) {
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': 
-            if (b == 0) {return false;}
-        return a / b;
+float apply_operator(float a, float b, char op){
+    switch (op)
+    {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': 
+    if(b == 0){
+        return false;
+    }
+    return a / b;
     }
 }
 
-int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
-}
+    int precedence(char op){
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
+    }
 
-
-float calc_infix(const string& expression) {
+float calc_infix(string expression){
     stack<float>operands;
     stack<char>operators;
 
     int i = 0;
     while (i < expression.length()){
         char token = expression[i];
-
         if (token == ' '){
-        i++;
-        continue;
+            i++;
+            continue;
         }
 
-        if(isdigit(token)){
+        else if (isdigit(token)){
             float num = 0;
-            while(i < expression.length() && isdigit(expression[i])){
-                num = num * 10 + (expression[i] - '0');
-                i++;
+            while(i < expression.length() && isdigit(token)){
+            num = num * 10 + (expression[i] - '0');
+            i++;
             }
             operands.push(num);
         }
@@ -49,38 +50,38 @@ float calc_infix(const string& expression) {
             i++;
         }
         else if (token == ')'){
-            while(!operators.empty() && operators.top() != '('){
-                float b = operands.top(); operands.pop();
-                float a = operands.top(); operands.pop();
-                char op = operators.top(); operators.pop();
-                operands.push(apply_operator(a, b, op));
+            while(!operands.empty() && operators.top() != '('){
+            float b = operands.top(); operands.pop();
+            float a = operands.top(); operands.pop();
+            char op = operators.top(); operators.pop();
+            operands.push(apply_operator(a, b, op));
             }
-            if (!operators.empty() && operators.top() == '('){
+            if (operators.top() == '('){
                 operators.pop();
             }
             i++;
         }
-        else if(token == '+' || token == '-' || token == '*' || token == '/'){
-            while (!operators.empty() && precedence(operators.top()) >= precedence(token)) {
+        else if (token == '+'|| token == '-'|| token == '*'|| token == '/'){
+            while(!operands.empty() && precedence(operators.top()) >= precedence(token)){
+            float b = operands.top(); operands.pop();
+            float a = operands.top(); operands.pop();
+            char op = operators.top(); operators.pop();
+            operands.push(apply_operator(a, b, op));
+            }
+        operators.push(token);
+        i++;
+         } else {
+            return false;
+        }
+        while(!operators.empty()){
             float b = operands.top(); operands.pop();
             float a = operands.top(); operands.pop();
             char op = operators.top(); operators.pop();
             operands.push(apply_operator(a, b, op));
         }
-        operators.push(token);
-        i++;
-        } else {
+        if(operands.size() != 1){
             return false;
         }
-    }
-    while (!operators.empty()){
-        float b = operands.top(); operands.pop();
-        float a = operands.top(); operands.pop();
-        char op = operators.top(); operators.pop();
-        operands.push(apply_operator(a, b, op));
-    }
-    if (operands.size() != 1){
-        return false;
     }
     return operands.top();
 }
