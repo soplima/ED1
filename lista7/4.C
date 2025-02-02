7367861c4196e52fb83fb8dbf3509e91c3c608cc
Sophia Lima <sop.lima13@gmail.com>
2025-02-02 20:47:32 -0300

    ex4

--- lista7/4.C
+++ lista7/4.C
@@ -1,14 +1,18 @@
+//Escreva uma função que recebe a referência de duas forward_list<int> e concatena seus conteúdos
+//em um vector<int>, a ser devolvido pelo função.
+
+
 #include <forward_list>
 #include <vector>
 using namespace std;
 
 vector<int> list_concat(forward_list<int> &list1, forward_list<int> &list2)
 {
-    vector<int>result; 
-    for(int val: list1){
+    vector<int>result;
+    for (int val: list1){
         result.push_back(val);
     }
-    for(int val: list2){
+    for (int val: list2){
         result.push_back(val);
     }
     return result;
