aa91f19c2df23494a75d2a866b912c7361eca1d6
Sophia Lima <sop.lima13@gmail.com>
2025-02-02 20:27:35 -0300

    ex1

--- lista7/1.C
+++ lista7/1.C
@@ -1,10 +1,12 @@
+//Escreva uma função que recebe o endereço de um array de inteiros e devolve um vector<int>,
+//contendo os elementos do array na mesma ordem.
+
 #include <vector>
 
 std::vector<int> vet_to_vector(int *v, int n)
 {
-    std::vector<int> result;
-    for (int i = 0; i < n; i++)
-    {
+    std::vector<int>result;
+    for (int i = 0; i < n; i ++){
         result.push_back(v[i]);
     }
     return result;
