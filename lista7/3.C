3704849fdce2d7ef6b552853bbb2930c5f1d60e7
Sophia Lima <sop.lima13@gmail.com>
2025-02-02 20:39:57 -0300

    ex3

--- lista7/3.C
+++ lista7/3.C
@@ -1,11 +1,14 @@
+//Escreva uma função que recebe o endereço de um array de inteiros e devolve uma stack<int>,
+//contendo os elementos do array na mesma ordem.
+
 #include <stack>
 using namespace std;
 
 stack<int> vet_to_stack(int *v, int n)
 {
-  stack<int>s;
-  for (int i = 0; i < n; i++){
-    s.push(v[i]);
+  stack<int>stack;
+  for (int i = 0; i < n; i ++){
+    stack.push(v[i]);
   }
-  return s;
+  return stack;
 }
