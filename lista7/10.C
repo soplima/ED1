bab4dffe9c6a065158b1c30663a8ed11fd2e524b
Sophia Lima <sop.lima13@gmail.com>
2025-02-03 14:35:45 -0300

    ex10

--- lista7/10.C
+++ lista7/10.C
@@ -1,3 +1,17 @@
+//Escreva um conversor de expressões posfix para infix, usando uma versão modificada da calculadora
+//posfix. Exemplo: 6 9 + 3 / 6 4 - * → ( ( ( 6 + 9 ) / 3 ) * ( 6 - 4 ) )
+
+//● Utilize uma pilha de strings (stack<string>) para guardar as expressões na forma infix.
+//● Utilize os recursos da biblioteca C/C++ para facilitar a construção do programa.
+//● Para avaliar a expressão deve-se seguir o processo abaixo:
+//o Quando um número (operando) é lido, ele é colocado na pilha.
+//o Quando um operador é lido, deve montar a expressão infix com os dois operandos
+//previamente empilhados: retira dois topos da pilha e monta expressão (string) na forma:
+//( topo operador topo ). Empilha a expressão infix na pilha.
+//● Ao final da expressão, a pilha deverá conter um único elemento, com a expressão completa na
+//forma infix.
+
+
 #include <iostream>
 #include <string>
 #include <stack>
@@ -7,37 +21,36 @@ using namespace std;
 
 string posfix_to_infix(string expression)
 {
-    stack<string>operand;
+    stack<string>operands;
 
     int i = 0;
-    while(i < expression.length()){
-
+    while (i < expression.length()){
         char token = expression[i];
-        if (token == ' '){
+
+        if(token == ' '){
             i++;
             continue;
         }
-
-        if(isdigit(token)){
-            string num = "";
-            while(i < expression.length() && isdigit(expression[i])){
-                num += expression[i];
-                i++;
-            }
-            operand.push(num);
+        else if(isdigit(token)){
+           string num = "";
+           while(i < expression.length() && isdigit(expression[i])){
+            num += expression[i];
+            i++;
+           }
+           operands.push(num);
         }
         else if (token == '+'|| token == '-'|| token == '*'|| token == '/'){
-            operand.size() < 2;
+            operands.size() < 2;
             throw invalid_argument("Expressão posfixa inválida.");
         }
-        string operand2 = operand.top(); operand.pop();
-        string operand1 = operand.top(); operand.pop();
-        string infix_exp = "(" + operand1 + " " + token + " " + operand2 + ")";
-        operand.push(infix_exp);
+        string operands2 = operands.top(); operands.pop();
+        string operands1 = operands.top(); operands.pop();
+        string infix_exp = "(" + operands1 + " " + token + " " + operands2 + ")";
+        operands.push(infix_exp);
         i++;
     }
-    if(operand.size() != 1){
-        throw invalid_argument ("Expressão posfixa inválida.");
+    if(operands.size() != 1){
+        throw invalid_argument("Expressão posfixa inválida.");
     }
-    return operand.top();
+    return operands.top();
 }
