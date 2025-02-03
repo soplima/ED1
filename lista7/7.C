0b8a66df33a70e791b74e7cddcaab9cf86a48b07
Sophia Lima <sop.lima13@gmail.com>
2025-02-02 22:06:36 -0300

    ex7

--- lista7/7.C
+++ lista7/7.C
@@ -1,3 +1,24 @@
+//Escreva uma função que calcula e devolve o resultado de expressões posfix usando os recursos da
+//biblioteca padrão de C/C++. Considere que sempre existe um espaço entre os elementos da
+//expressão e que os números são inteiros positivos e podem ser formados por mais de um algarismo.
+//Operações possíveis: + - * /. É necessário usar uma pilha para os operandos (stack<float>).
+
+//Recursos:
+//● isdigit(): verifica se caractere é um dígito (número)
+//● stoi(): converte string C++ para número (http://www.cplusplus.com/reference/string/stoi/)
+
+//Processo:
+//● Transforme a expressão string em um vector<string>. Desta forma, cada símbolo (operador ou
+//operando) estará em uma posição do vector.
+//● Varrer a expressão no vector:
+//▪ Quando encontra um número na expressão, empilha (stack<float>).
+//▪ Quando encontra um operador na expressão, retira dois elementos da pilha, executa a
+//operação e empilha resultado (a ordem dos operandos deve ser considerada na
+//execução da operação);
+//▪ Quando terminar de percorrer a expressão, o resultado estará no topo da pilha
+
+
+
 #include <string>
 #include <vector>
 #include <stack>
@@ -8,7 +29,7 @@ using namespace std;
 
 vector<string> vectorize_expression(const string& expression) {
     vector<string>result;
-    stringstream ss (expression);
+    stringstream ss;
     string token;
 
     while(ss >> token){
@@ -20,34 +41,34 @@ float calc_posfix(string expression){
     stack<float>operand;
     vector<string>tokens = vectorize_expression(expression);
 
-    for (const string& token: tokens){
+    for (const string token: tokens){
         if(isdigit(token[0])){
-            operand.push(stof(token));
+            operand.push(stoi(token));
         }
-        else {
-            if(operand.size() < 2){
+
+        else if (operand.size() < 2){
             return false;
         }
-            float b = operand.top(); operand.pop();
-            float a = operand.top(); operand.pop();
-            
-            if(token == "+"){
-                operand.push(a + b);
-            }
-            if(token == "-"){
-                operand.push(a - b);
-            }
-            if(token == "*"){
-                operand.push(a * b);
-            }
-            if(token == "/"){
-                if(b == 0){
-                    return false;
-                }
-                operand.push(a / b);
-            }
+
+        float b = operand.top(); operand.pop();
+        float a = operand.top(); operand.pop();
+
+        if(token == "+"){
+            return a + b;
         }
-        if(operand.size() != 1){
+        if(token == "-"){
+            return a - b;
+        }
+        if(token == "*"){
+            return a * b;
+        }
+        if(token == "/"){
+            if(b == '0'){
+                return false;
+            }
+            return a / b;
+        }
+        if (operand.size() != 1){
             return false;
         }
     }
