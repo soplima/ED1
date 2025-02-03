b0dac547fb4ccbebb12f9e1fecca56dac0078dc6
Sophia Lima <sop.lima13@gmail.com>
2025-02-02 21:14:07 -0300

    ex6

--- lista7/6.C
+++ lista7/6.C
@@ -1,3 +1,10 @@
+//Escreva uma função que recebe uma expressão pós-fixada (notação Polonesa Inversa) como uma
+//string e devolve um vector<string> contendo cada elemento da expressão. Note que existe um
+//caractere de espaço entre cada elemento da expressão e que os números podem ser compostos por
+//mais de um dígito. Utilize os recursos sugeridos da biblioteca padrão de C++.
+//Exemplo: entrada: "24 32 + 2 / 41 5 * +"
+//saída: vector<string> = {24, 32, +, 2, /, 41, 5, *, +}
+
 #include <string>
 #include <vector>
 #include <sstream>
@@ -5,12 +12,12 @@ using namespace std;
 
 vector<string> vectorize_expression(const string &expression)
 {
-    vector<string>result;
+    vector<string>s;
     stringstream ss (expression);
     string token;
 
     while(ss >> token){
-        result.push_back(token);
+        s.push_back(token);
     }
-    return result;
+    return s;
 }
