c27cc92af14c20e1c7a2954334f97e968bc0181c
Sophia Lima <sop.lima13@gmail.com>
2025-02-10 18:34:26 -0300

    ex9

--- lista7/2.C
+++ lista7/2.C
@@ -1,24 +1,17 @@
-//Escreva uma função que recebe o endereço de um array de inteiros e devolve uma forward_list<int>,
-//contendo os elementos do array na mesma ordem.
+//Escreva uma função que recebe o endereço de um array de inteiros e devolve uma list<int>, contendo
+//os elementos do array na mesma ordem.
 
-#include <forward_list>
+
 #include <iostream>
+#include <list>
 
-std::forward_list<int> vet_to_flist(int *v, int n)
-{
-    std::forward_list<int>flist;
+using namespace std;
 
-    if(n == 0 || v == nullptr){
-        return flist;
+list<int> vet_to_list(int* v, int n){
+    list<int>result;
+    for(int i = 0; i < n; i ++){
+    result.push_back(v[i]);
     }
-    auto it = flist.before_begin();
-    for (int i = 0; i < n; i ++){
-        it = flist.insert_after(it, v[i]);
-    }
-    return flist;
+    return result;
 }
 
-//flist.insert_after(flist.before_begin)
-//insere depois de antes do comeco
-//ex 1, 2, 3, 4, 5
-// after: 0, 1 2 3 4 5
--- lista7/9.C
+++ lista7/9.C
@@ -1,105 +1,86 @@
-//Escreva uma calculadora de expressões infix parentizada: ( ( ( 6 + 9 ) / 3 ) * ( 6 - 4) )
-
-//● Considere que sempre existe um espaço entre os elementos da expressão e que ela está
-//completamente parentizada para informar a ordem de precedência das operações.
-//● São necessárias duas pilhas: operadores (stack <char>) e operandos (stack <float>)).
-//● Utilize novamente os recursos da biblioteca C/C++ para facilitar a construção do programa.
-//● Para avaliar a expressão deve-se seguir o processo abaixo:
-
-//o Quando um operando (número) é lido, ele é colocado na pilha de operandos.
-//o Quanto um parêntese esquerdo é lido "(", ele é colocado na pilha de operadores.
-//o Quando um dos quatro operadores é lido, ele é colocado na pilha de operadores caso
-//tenha maior precedência sobre o operador que está no topo da pilha ou topo seja “(“.
-//▪ Caso contrário, deve calcular a expressão empilhada: retirar dois números da
-//pilha de operandos e o operador da outra pilha. Calcular a expressão e colocar o
-//resultado na pilha de operandos.
-//o Quando um parêntese direito é lido ")", deve calcular a expressão empilhada seguindo o
-//processo mencionado acima, colocando o resultado na pilha de operandos. Este
-//processo é repetido até que o topo da pilha de operadores contenha um parêntese
-//esquerdo "(", que deverá ser removido.
-
-
 #include <iostream>
 #include <string>
 #include <stack>
+#include <sstream>
 #include <cctype>
+#include <vector>
 #include <stdexcept>
 using namespace std;
 
-float apply_operators(float a, float b, char op){
-    switch (op)
-    {
+float apply_operands(float a, float b, char op){
+    switch(op){
     case '+': return a + b;
     case '-': return a - b;
     case '*': return a * b;
-    case '/':
-    if(b == '0'){
-        return false;
-    } 
+    case '/': 
+    if(b == 0){
+        throw runtime_error("Division by zero");
+    }
     return a / b;
     }
 }
-int precendence(char op){
+int precedence (char op){
     if(op == '+' || op == '-') return 1;
     if(op == '*' || op == '/') return 2;
     return 0;
 }
 
 float calc_infix(string expression){
+    stack <char>operators;
     stack<float>operands;
-    stack<char>operators;
 
     int i = 0;
-    while (i < expression.length()){
+    while(i < expression.length()){
         char token = expression[i];
+
         if (token == ' '){
             i++;
             continue;
         }
-        else if (isdigit(token)){
+
+        else if(isdigit(token)){
             float num = 0;
-            while(i < expression.length() && isdigit(expression[i])){
+            while (i<expression.length() && isdigit(expression[i])){
                 num = num * 10 + (expression[i] - '0');
                 i++;
             }
             operands.push(num);
         }
-        else if(token == '('){
+        if(token == '('){
             operators.push(token);
             i++;
         }
-        else if(token == ')'){
-            while(!operators.empty() && operators.top() != '('){
+        if(token == ')'){
+            while(!operands.empty() && operators.top()!= '('){
                 float b = operands.top(); operands.pop();
                 float a = operands.top(); operands.pop();
                 char op = operators.top(); operators.pop();
-                operands.push(apply_operators(a, b, op));
-            }
-            if (operators.top() == '('){
+                operands.push(apply_operands(a, b, op));
+            } 
+            if(operators.top() == '('){
                 operators.pop();
-            }
+            } 
             i++;
         }
-        else if(token == '+' || token == '-' || token == '*' || token == '/'){
-            while(!operators.empty() && precendence(operators.top()) >= precendence(token)){
+        if(token == '+' || token == '-' || token == '*' || token == '/'){
+            while(!operators.empty() && precedence(operators.top()) >= precedence(token)){
                 float b = operands.top(); operands.pop();
                 float a = operands.top(); operands.pop();
                 char op = operators.top(); operators.pop();
-                operands.push(apply_operators(a, b, op));
+                operands.push(apply_operands(a, b, op));
             }
             operators.push(token);
             i++;
         }
-
-    while(!operators.empty()){
-        float b = operands.top(); operands.pop();
-        float a = operands.top(); operands.pop();
-        char op = operators.top(); operators.pop();
-        operands.push(apply_operators(a, b, op));
+        }
+        while(!operators.empty()){
+            float b = operands.top(); operands.pop();
+            float a = operands.top(); operands.pop();
+            char op = operators.top(); operators.pop();
+            operands.push(apply_operands(a, b, op));
+        }
+        if(operands.size() != 1){
+            return false;
+        }
+        return operands.top();
     }
-    if(operands.size() != 1){
-        throw runtime_error("Invalid expression");
-    }
-    return operands.top();
-}
-}
