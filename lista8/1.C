fa99c33f9754ccd6c74b4c5a32b1fb77c7bf4944
Sophia Lima <sop.lima13@gmail.com>
2025-02-10 22:13:38 -0300

    ex1

---
+++ lista8/1.C
@@ -0,0 +1,11 @@
+//Escreva uma função recursiva que imprime 10 vezes a mesma linha de texto.
+
+#include <iostream>
+using namespace std;
+
+void print_txt(char* text, int count = 10) {
+    if (count == 0) return;  
+    cout << text << endl;    
+    print_txt(text, count - 1);  
+}
+
---
+++ lista8/2.C
@@ -0,0 +1,10 @@
+//Escreva uma função recursiva que imprime uma contagem de 0 à 50.
+
+#include <iostream>
+using namespace std;
+
+void count(int num){
+    if (num > 50 ) return;
+    cout << num << " ";
+    count (num + 1);
+}
