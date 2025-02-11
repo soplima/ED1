4a0961993efb0f985a0563621f27fe11eb5c3850
Sophia Lima <sop.lima13@gmail.com>
2025-02-11 12:54:12 -0300

    ex10

---
+++ lista8/10.C
@@ -0,0 +1,14 @@
+//Escreva uma função recursiva que desempilha todos os elementos da 
+//stack1 e os empilha, em ordem inversa, na stack2.
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void push_to(stack<int>& stk1, stack<int>& stk2){
+    if(stk1.empty())return;
+    int top = stk1.top();
+    stk1.pop();
+    push_to(stk1, stk2);
+    stk2.push(top);
+}
