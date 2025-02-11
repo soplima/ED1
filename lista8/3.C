8befafe65fdfc39ebc6664be5ee530fe2215eca5
Sophia Lima <sop.lima13@gmail.com>
2025-02-10 22:26:58 -0300

    ex3

---
+++ lista8/3.C
@@ -0,0 +1,10 @@
+//Escreva uma função recursiva que imprime uma contagem de 0 à max.
+
+#include <iostream>
+using namespace std;
+
+void count_max(int num, int max){
+    if(num > max) return;
+    cout << num << " ";
+    count_max(num + 1, max);
+}
