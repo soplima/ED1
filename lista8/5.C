fecab184b039f2536f9291b71fd8e414171fb07c
Sophia Lima <sop.lima13@gmail.com>
2025-02-10 22:52:59 -0300

    ex5

---
+++ lista8/5.C
@@ -0,0 +1,14 @@
+//Escreva uma função recursiva que imprime todos os elementos pares de uma 
+//Lista Sequencial (vector), em ordem inversa.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print_even(vector<int>& vec, int idx){
+    if (idx < 0) return;
+    print_even(vec, idx - 1);
+    if(vec[idx] % 2 == 0){
+    cout << vec[idx] << " ";
+    }
+}
