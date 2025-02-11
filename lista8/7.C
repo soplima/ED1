937be2a85c77c510616090b6d483ba181b408d87
Sophia Lima <sop.lima13@gmail.com>
2025-02-11 11:12:51 -0300

    ex7

---
+++ lista8/7.C
@@ -0,0 +1,16 @@
+//Escreva uma função recursiva que imprime todos os elementos de uma Lista 
+//Simplesmente Encadeada (forward_list), em ordem inversa.
+
+#include <iostream>
+#include <forward_list>
+
+using namespace std;
+
+void print_rev(forward_list<int>& lst, forward_list<int>::iterator& it){
+    if(it == lst.end()) return;
+    auto next_it = it;
+    ++next_it; 
+    print_rev(lst, next_it);
+    cout << *it << " ";
+
+}
