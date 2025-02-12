77b8a97316f797f4ce3967c4bad6e7f2517853ac
Sophia Lima <sop.lima13@gmail.com>
2025-02-12 12:13:43 -0300

    ex1

---
+++ lista9/1.C
@@ -0,0 +1,12 @@
+//Escreva uma função recursiva que retorna a soma de todos os 
+//elementos de um vector de tamanho n que estão em posições pares.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int sum(vector<int>& vec, int n){
+    if(n < 0) return;
+    int s = (n % 2 == 0) ? vec[n] : 0;
+    return s + sum(vec, n - 1);
+}
