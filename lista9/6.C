f590d5ddab330379eb9198d39583ff1651b2fed0
Sophia Lima <sop.lima13@gmail.com>
2025-02-12 20:25:59 -0300

    ex6

---
+++ lista9/6.C
@@ -0,0 +1,12 @@
+//Escreva uma função recursiva que retorna a quantidade de 
+//ocorrência de um elemento em um vectorv de tamanho n.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int count(vector<int>& vec, int n, int elem){
+    if(n == 0) return 0;
+    return (vec[n - 1] == elem) + count(vec, n - 1, elem);
+
+}
