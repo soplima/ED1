bedb24111c68a4f4dedcb54afb95ea7b228a3ff9
Sophia Lima <sop.lima13@gmail.com>
2025-02-12 13:24:05 -0300

    ex2

---
+++ lista9/2.C
@@ -0,0 +1,11 @@
+//Escreva uma função recursiva que retorna o produto de todos os 
+//elementos de uma list de início em it.
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int product(list<int>& lst, list<int>::iterator it){
+    if(it == lst.end())return 1;
+    return  (*it) * product(lst, next(it));
+}
