74da28bf3e244e055003a123a20f44b28e1b4113
Sophia Lima <sop.lima13@gmail.com>
2025-02-12 19:49:47 -0300

    ex4

---
+++ lista9/4.C
@@ -0,0 +1,12 @@
+//Escreva uma função recursiva que retorna o menor dos 
+//elementos de uma lista de início em it.
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int min(list<int>& lst, list<int>::iterator it){
+    if(it == lst.end()) return INT_MAX;
+    int menor = min(lst, next(it));
+    return (*it < menor)? *it : menor;
+}
