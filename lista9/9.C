8dd69bfcb89aa8c7e151c46a5ccfd28a2884d4f7
Sophia Lima <sop.lima13@gmail.com>
2025-02-13 14:01:30 -0300

    ex9

---
+++ lista9/9.C
@@ -0,0 +1,11 @@
+//Escreva uma função recursiva que verifica se uma string forma um 
+//palíndromo.
+
+#include <iostream>
+using namespace std;
+
+bool palindrome(string& str, int first, int last) {
+    if(first >= last) return true;
+    if(str[first] != str[last]) return false;
+    palindrome(str, first + 1, last - 1);
+}
