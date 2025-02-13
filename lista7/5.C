1374893a90213f6295ec7b7e627392a98ec1bef8
Sophia Lima <sop.lima13@gmail.com>
2025-02-13 17:42:36 -0300

    ex5

--- lista7/5.C
+++ lista7/5.C
@@ -1,40 +1,28 @@
-//Escreva uma função que verifica a parentização de expressões string infix do tipo “[(x + 8) * (9-2)]”.
-//Utilize uma pilha stack<char> para realizar o processo:
-//● percorrer a expressão de entrada
-//● ao encontrar um símbolo "abre", ( [ { , empilha
-//● ao encontrar um símbolo "fecha", ) ] }, retira o símbolo do tipo "abre" do topo e compara ambos
-//o se o "abre" e o "fecha" correspondem, continua
-//o se diferem, então a expressão está incorreta
-//o se a pilha estiver vazia, a expressão está incorreta
-//● ao terminar de percorrer a expressão de entrada, a pilha deve estar vazia
-//o se restou algum símbolo na pilha, então a expressão está incorreta
-
-
 #include <stack>
 #include <string>
 using namespace std;
 
-bool check_brackets(const string &expression)
-{
-    stack<char>operators;
-
-    for(char ch: expression){
-        if(ch == '(' || ch == '[' || ch == '{'){
-            operators.push(ch);
+bool check_brackets(const string &expression) {
+    stack<char> operators;
+    
+    for (const char &token : expression) {
+        if (token == '(' || token == '[' || token == '{') {
+            operators.push(token);
+        } 
+        else if (token == ')' || token == ']' || token == '}') {
+            if (operators.empty()) {
+                return false; 
+            }
+            char top = operators.top();
+            operators.pop();
+            
+            if ((token == ')' && top != '(') ||
+                (token == ']' && top != '[') ||
+                (token == '}' && top != '{')) {
+                return false;
+            }
         }
-        else if (ch == ')' || ch == ']' || ch == '}'){
-        if(operators.empty())
-            return false;
-        
-        }
-        char top = operators.top();
-        operators.pop();
-        
-        if((ch == ')' && top != '(') ||
-           (ch == ']' && top != '[') ||
-           (ch == '}' && top != '{')){
-            return false;
-           }
     }
+
     return operators.empty();
 }
