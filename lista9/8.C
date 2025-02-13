7867acd09072ed047009e599f2e582780858e066
Sophia Lima <sop.lima13@gmail.com>
2025-02-13 13:31:02 -0300

    ex8

---
+++ lista9/8.C
@@ -0,0 +1,13 @@
+//Escreva uma função recursiva que inverte os elementos em uma list, 
+//de início em first e final em last.
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+void reverse(list<int>& lst, list<int>::iterator first, 
+    list<int>::iterator last){
+        if (first == last || next(first) == last) return;
+        swap(*first, *last);
+        reverse(lst, next(first), prev(last));
+}
