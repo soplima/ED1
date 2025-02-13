3d3cec4ba6fa1c889069c3aac2f8e3db4cfdbe47
Sophia Lima <sop.lima13@gmail.com>
2025-02-13 18:50:24 -0300

    ex8

--- lista7/8.C
+++ lista7/8.C
@@ -18,25 +18,27 @@ vector<string> vectorize_expression(const string &expression)
     vector<string>result;
     stringstream ss (expression);
     string token;
-
     while(ss >> token){
         result.push_back(token);
     }
     return result;
 }
+
 bool check_posfix(string expression){
     stack<float>operands;
     vector<string>tokens = vectorize_expression (expression);
 
-    for(const string token: tokens){
+    for(const string &token: tokens){
         if(isdigit(token[0])){
             operands.push(1);
         }
-        else
-            if(operands.size() < 2){
-                return false;
-            }
-            operands.pop();
+        else 
+        if(operands.size() < 2){
+            throw runtime_error("insuficient operands");
+        }
+        else{
+        operands.pop();
+        }
     }
     return operands.size() == 1;
 }
