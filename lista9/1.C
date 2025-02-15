c768bcdbe9eb63659d87a3c560d732c200224405
Sophia Lima <sop.lima13@gmail.com>
2025-02-15 17:37:44 -0300

    ex1

--- lista9/1.C
+++ lista9/1.C
@@ -6,7 +6,7 @@
 using namespace std;
 
 int sum(vector<int>& vec, int n){
-    if(n == 0) return 0;
-    int s = (n % 2 == 0)? vec[n] : 0;
-    return s + sum (vec, n - 1);
+    if(n < 0) return 0;
+    int s = (n % 2 == 0)? vec[n]: 0;
+    return s + sum(vec, n -1);
 }
