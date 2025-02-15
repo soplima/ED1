62e1b37c5435397c5f0c14c4b6f726c0e33d1caa
Sophia Lima <sop.lima13@gmail.com>
2025-02-15 15:05:54 -0300

    ex7

--- lista8/7.C
+++ lista8/7.C
@@ -3,14 +3,12 @@
 
 #include <iostream>
 #include <forward_list>
-
 using namespace std;
 
 void print_rev(forward_list<int>& lst, forward_list<int>::iterator& it){
     if(it == lst.end()) return;
     auto next_it = it;
-    ++next_it; 
+    next_it++;
     print_rev(lst, next_it);
-    cout << *it << " ";
-
+    cout << *it << endl;
 }
