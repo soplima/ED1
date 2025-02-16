73c0fb9b5fda25a265e01e867428db7b9924a1d8
Sophia Lima <sop.lima13@gmail.com>
2025-02-16 05:16:35 -0300

    ex9

--- lista9/9.C
+++ lista9/9.C
@@ -6,6 +6,6 @@ using namespace std;
 
 bool palindrome(string& str, int first, int last) {
     if(first >= last) return true;
-    if(str[first] != str[last]) return false;
-    return palindrome(str, first + 1, last - 1);
+    if(str[first] != str[last])return false;
+    palindrome(str, first+1, last-1);
 }
