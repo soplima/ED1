07400150cb5631f73158747a027f029b36ed0332
Sophia Lima <sop.lima13@gmail.com>
2025-02-16 05:03:17 -0300

    ex7

--- lista9/7.C
+++ lista9/7.C
@@ -8,5 +8,5 @@ using namespace std;
 void reverse(vector<int>& vec, int first, int last) {
     if(first >= last) return;
     swap(vec[first], vec[last]);
-    reverse(vec, first + 1, last -1);
+    reverse(vec, first+1, last-1);
 }
