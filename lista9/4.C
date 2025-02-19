fe491607c95f80ac746029a5c1ee97f770e9788b
Sophia Lima <sop.lima13@gmail.com>
2025-02-19 15:45:14 -0300

    ex5 l9

--- lista9/4.C
+++ lista9/4.C
@@ -5,8 +5,7 @@
 #include <list>
 using namespace std;
 
-int min(list<int>& lst, list<int>::iterator it)
-{
+int min(list<int>& lst, list<int>::iterator it){
     if(it == lst.end()) return INT_MAX;
     int menor = min(lst, next(it));
     return(*it < menor)? *it : menor;
--- lista9/5.C
+++ lista9/5.C
@@ -5,8 +5,9 @@
 #include <vector>
 using namespace std;
 
-bool find(vector<int>& vec, int n, int elem){
-    if(n < 0) return false;
-    if (vec[n] == elem) return true;
+bool find(vector<int>& vec, int n, int elem)
+{
+    if(n > 0) return false;
+    if(vec[n] == elem) return true;
     return find(vec, n -1, elem);
 }
