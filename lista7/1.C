fb437d9841491a927abd5082ad2c7b1351bf2fff
Sophia Lima <sop.lima13@gmail.com>
2025-02-19 12:24:25 -0300

    ex1 l7

--- lista7/1.C
+++ lista7/1.C
@@ -3,8 +3,7 @@
 
 #include <vector>
 
-std::vector<int> vet_to_vector(int *v, int n)
-{
+std::vector<int> vet_to_vector(int *v, int n){
     std::vector<int>result;
     for(int i = 0; i < n; i ++){
         result.push_back(v[i]);
