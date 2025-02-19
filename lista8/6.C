23d75ea68514a5fa25c52e3d21bfb79633031dcb
Sophia Lima <sop.lima13@gmail.com>
2025-02-19 15:03:38 -0300

    ex6 l8

--- lista8/6.C
+++ lista8/6.C
@@ -5,8 +5,9 @@
 #include <list>
 using namespace std;
 
-void print_list(list<int>& lst, list<int>::iterator it) {
-    if (it == lst.end()) return;
+void print_list(list<int>& lst, list<int>::iterator it) 
+{
+    if(it == lst.end())return;
     cout << *it << endl;
-    print_list(lst, next(it)); 
+    print_list(lst, next(it));
 }
