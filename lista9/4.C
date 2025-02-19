d26e7c055035e6fc01550509c0365784c54f08f0
Sophia Lima <sop.lima13@gmail.com>
2025-02-19 15:41:35 -0300

    ex4 l9

--- lista9/4.C
+++ lista9/4.C
@@ -9,5 +9,5 @@ int min(list<int>& lst, list<int>::iterator it)
 {
     if(it == lst.end()) return INT_MAX;
     int menor = min(lst, next(it));
-    return (*it < menor)? *it : menor;
+    return(*it < menor)? *it : menor;
 }
